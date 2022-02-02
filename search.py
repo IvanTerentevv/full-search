bda75fbb8aebde396bc907116941f5e8489df3d9
IvanTerentevv <94695795+IvanTerentevv@users.noreply.github.com>
2022-02-02 20:48:36 +0300

    Update search.py

--- search.py
+++ search.py
@@ -1,20 +1,16 @@
-import pygame
+import sys
+from io import BytesIO
 import requests
+from PIL import Image
+
 import find
 
 
 map_api_server = "http://static-maps.yandex.ru/1.x/"
-map_params = find.params()
+map_params = find.params(" ".join(sys.argv[1:]))
 response = requests.get(map_api_server, params=map_params)
 
 map_file = "map.png"
 open(map_file, "wb").write(response.content)
 
-pygame.init()
-screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
-pygame.draw.circle(screen, pygame.Color('red'), (300, 225), 5, width=0)
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
+Image.open(BytesIO(response.content)).show()
