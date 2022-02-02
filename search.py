ce7d30192012989a0f0e2d19c04d11b17858d16b
IvanTerentevv <94695795+IvanTerentevv@users.noreply.github.com>
2022-02-02 21:58:00 +0300

    Update search.py

--- search.py
+++ search.py
@@ -10,7 +10,4 @@ map_api_server = "http://static-maps.yandex.ru/1.x/"
 map_params = find.params(" ".join(sys.argv[1:]))
 response = requests.get(map_api_server, params=map_params)
 
-map_file = "map.png"
-open(map_file, "wb").write(response.content)
-
 Image.open(BytesIO(response.content)).show()
